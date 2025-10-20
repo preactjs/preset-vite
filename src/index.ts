5ee464ec8baa63b6c61538db82f16f8ac3ed645a
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2025-10-20 11:24:15 -0500

    fix: Add jsx-runtime to aliases (#171)

--- src/index.ts
+++ src/index.ts
@@ -272,6 +272,7 @@ function preactPlugin({
 									alias: {
 										"react-dom/test-utils": "preact/test-utils",
 										"react-dom": "preact/compat",
+                                        "react/jsx-runtime": "preact/jsx-runtime",
 										react: "preact/compat",
 									},
 								},
