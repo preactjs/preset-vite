b4c16384352c6bd284d505c9038b36fff60c9cad
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2025-11-09 00:23:07 -0600

    chore: Bump main deps (#179)
    * chore: Bump deps
    
    * chore: Switch to newer TS module resolution
    
    * fix: skipLibCheck to get around Rollup's broken plugin types
    
    * test: Correct test utils

--- test/dev.test.js
+++ test/dev.test.js
@@ -1,4 +1,3 @@
-import assert from "node:assert";
 import { afterEach, beforeEach, describe, it } from "node:test";
 import { devServerURL, launchDemoDevServer } from "./util.js";
 
@@ -9,33 +8,17 @@ describe("dev server", async () => {
 	});
 
 	afterEach(() => {
-		// Until nodejs/node#45204 is released in node v18, this won't be called on
-		// test failures, so we'll need to manually wrap tests for now
 		devServerProc.kill();
 	});
 
-	// TODO: Remove this wrapper once nodejs/node#45204 is released
-	const wrap = fn => {
-		return async (...args) => {
-			try {
-				await fn(...args);
-			} finally {
-				devServerProc.kill();
-			}
-		};
-	};
-
-	it(
-		"serves src/main.tsx",
-		wrap(async () => {
-			const mainURL = new URL("/src/index.tsx", devServerURL);
-			const res = await fetch(mainURL);
-			if (!res.ok) {
-				const body = await res.text();
-				throw new Error(
-					`Response for ${mainURL} indicated failure. Status code: ${res.status}. Full response:\n${body}`,
-				);
-			}
-		}),
-	);
+	it("serves src/index.tsx", async () => {
+		const mainURL = new URL("/src/index.tsx", devServerURL);
+		const res = await fetch(mainURL);
+		if (!res.ok) {
+			const body = await res.text();
+			throw new Error(
+				`Response for ${mainURL} indicated failure. Status code: ${res.status}. Full response:\n${body}`,
+			);
+		}
+	});
 });
